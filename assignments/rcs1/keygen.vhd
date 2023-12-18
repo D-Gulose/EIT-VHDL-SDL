d85216e11167b12c95ec71c50de48152e95ef188
D-Gulose <goddessphilotes@protonmail.com>
2023-12-18 13:15:33 +0100

    -

--- assignments/rcs1/keygen.vhd
+++ assignments/rcs1/keygen.vhd
@@ -21,15 +21,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.std_logic_arith.all;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity keygen is
     Port ( inputkey : in  STD_LOGIC_VECTOR (127 downto 0);
            lap : in  STD_LOGIC_VECTOR (3 downto 0);
@@ -46,45 +37,79 @@ architecture Behavioral of keygen is
 	type matrix is array(51 downto 0) of std_logic_vector(15 downto 0);
 	signal subkeys : matrix := (others => (others => '0'));
 	signal shift_a, shift_b : std_logic;
-	signal keys_a, keys_b : std_logic_vector(1 downto 0);
+	signal keys_a, keys_b: std_logic_vector(1 downto 0);
 	
 	begin
 	
-		case lap is
-			when b"0000" => -- lap 1
-			when b"0001" => -- lap 2
-			when b"0010" => -- lap 3
-			when b"0011" => -- lap 4
-			when b"0100" => -- lap 5
-			when b"0101" => -- lap 6
-			when b"0110" => -- lap 7
-			when b"0111" => -- lap 8
-			when b"1000" => -- output trafo
-			when others => -- undefined
-		end case;
-	
-	
+		select_round: process
+			begin
+				case lap is
+					when b"0000" => -- lap 1
+						shift_a <= '0'; 
+						keys_a <= x"6_1"'; 
+						shift_b <= 'U'; 
+						keys_b <= x"XX";
+						
+					when b"0001" => -- lap 2
+						shift_a <= '0'; keys_a <= x"8_7";
+						shift_b <= '1'; keys_b <= x"4_1";
+						
+					when b"0010" => -- lap 3
+						shift_a <= '1'; keys_a <= x"8_5";
+						shift_b <= '2'; keys_b <= x"2_1";
 
+					when b"0011" => -- lap 4
+						shift_a <= '2'; keys_a <= x"8_3";
+						shift_b <= 'X'; keys_b <= "XX";
+						
+					when b"0100" => -- lap 5
+						shift_a <= '3'; keys_a <= x"6_1";
+						shift_b <= 'X'; keys_b <= "XX";
+						
+					when b"0101" => -- lap 6
+						shift_a <= '3'; keys_a <= x"8_7";
+						shift_b <= '4'; keys_b <= x"4_1";
+
+					when b"0110" => -- lap 7
+						shift_a <= '4'; keys_a <= x"8_5";
+						shift_b <= '5'; keys_b <= x"2_1";
+						
+					when b"0111" => -- lap 8
+						shift_a <= '5'; keys_a <= x"8_3";
+						shift_b <= 'X'; keys_b <= "XX";
+						
+					when b"1000" => -- output trafo
+						shift_a <= '6'; keys_a <= x"4_1";
+						shift_b <= 'X'; keys_b <= "XX";
+					
+					when others => -- undefined
+						report "Case error"
+				end case;
+		end process;				
+	
+	
 		process(KEY)
-			constant shifts : integer := 6;
-			constant partitions : integer := 7;
 			constant key_sz : integer:= 16;
 			variable key_shifted : std_logic_vector(127 downto 0); 
 			variable ctr : integer;
 				
-		begin
-		-- VARIABLES/SIGNALS ONLY DECLARE BEFORE 'BEGIN' AND ASSIGN AFTER 'BEGIN' !!!
-		ctr := 0;
-		key_shifted := KEY;		
-	
-		for shift in 0 to shifts loop 
-			for part in 0 to partitions loop 
-				subkeys(ctr) <= key_shifted((key_sz*8-1)-key_sz*part downto (key_sz*8)-key_sz*(part+1));
-				ctr := ctr + 1;
-				exit when ctr > 51 ;
+			begin
+			-- VARIABLES/SIGNALS ONLY DECLARE BEFORE 'BEGIN' AND ASSIGN AFTER 'BEGIN' !!!
+			ctr := 0;
+			key_shifted := KEY;		
+		
+			for shift in 0 to 6 loop 
+				for key in 0 to 8 loop
+					if shift = shift_a then
+						
+					if shift = shift_b then 
+					
+					subkeys(ctr) <= key_shifted((key_sz*8-1)-key_sz*part downto (key_sz*8)-key_sz*(part+1));
+					ctr := ctr + 1;
+					exit when ctr > 51 ;
+				end loop;
+				key_shifted := key_shifted(102 downto 0) & key_shifted(127 downto 103);
 			end loop;
-			key_shifted := key_shifted(102 downto 0) & key_shifted(127 downto 103);
-		end loop;
 		
 	end process;
 
